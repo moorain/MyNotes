450ae431d12afca4bffec2b905d64afbe2051449
moorain <1062254476@qq.com>
2017-07-24 16:07:21 +0800

    2
    2完成

---
+++ gallery/js/showPic.js
@@ -0,0 +1,6 @@
+
+function showPic(whichpic){
+    var source=whichpic.getAttribute("href");
+    var placeholder=document.getElementById("placeholder");
+    placeholder.setAttribute("src",source);
+}
