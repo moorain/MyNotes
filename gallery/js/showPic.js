c250f4c2b65c3f4e0e4230c94c0632c82e284762
moorain <1062254476@qq.com>
2017-07-25 08:59:59 +0800

    3
    增加了一个弹出窗口，显示通过将body元素传给element后的length属性，并且使用window.onload使函数使页面在加载时加载函数。

--- gallery/js/showPic.js
+++ gallery/js/showPic.js
@@ -4,3 +4,9 @@ function showPic(whichpic){
     var placeholder=document.getElementById("placeholder");
     placeholder.setAttribute("src",source);
 }
+function countBodyChildren(){
+    var body_element=document.getElementsByTagName("body")[0];//得到body的元素，存储到数组中；
+    // 变 量body-enement已经指向了文档的body元素.
+    alert(body_element.childNodes.length);//弹出窗口显示数组所包含的个数。
+}
+window.onload=countBodyChildren;//让这个函数在页面加载时执行。放在代码后。
