971c125231e0db54a594de76f9df8f00c4bd0451
Moorain <1062254476@qq.com>
2018-09-14 12:04:01 +0800

    Update example.js

--- DEMO/example.js
+++ DEMO/example.js
@@ -1,6 +1,9 @@
 // 数组去重
-
   uniq(arr) {
     const tmp = new Set(arr.map((item) => JSON.stringify(item)));
     return Array.from(tmp).map((item) => JSON.parse(item));
   }
+
+//数据计算相关
+//https://github.com/nefe/number-precision/blob/master/src/index.ts
+
