2dc67b31ad8a612153b1fe4063ad936c83dc4ec4
Moorain <1062254476@qq.com>
2018-12-27 12:09:20 +0800

    Update 数组相关.js

--- DEMO/数组相关.js
+++ DEMO/数组相关.js
@@ -40,4 +40,64 @@ arr1.concat(arr2, arr3);
 // [ 'a', 'b', 'c']
 
 
+//一个树形json的数据处理 传入id获取name 
+    const arr1 = [
+      {
+        id: '1',
+        name: 'a1',
+        children: [
+          {
+            id: '1-1',
+            name: 'a11',
+            children: [
+              {
+                id: '1-1-1',
+                name: 'a111',
+                children: [
+                ]
+              }
+            ]
+          }
+        ]
+      }, {
+        id: '2',
+        name: 'a2',
+        children: [
+          {
+            id: '2-1',
+            name: 'a21',
+            children: [
+              {
+                id: '2-1-1',
+                name: 'a211',
+              }
+            ]
+          }
+        ]
+      }
+    ]
+    
+    const getName = (id, data) => {
+      const idArr = id.split('-')
+      let j = 1;
+      let name = null;
+      const named = (list) => {
+        const theId = idArr.slice(0, j).join('-');
+        for (let i = 0; i < list.length; i++) {
+          const item = list[i];
+          if (item.id === theId) {
+            if (id === item.id) {
+              name = item.name;
+              break;
+            } else {
+              j++;
+              item.children && named(item.children)
+            }
+          }
+        }
+        return name;
+      }
+      return named(data);
+    }
+    console.log(getName('1-1', arr1))   //a11
 
