30731900a7b378d7b08a119720c977c0e6f1f4c7
Moorain <1062254476@qq.com>
2019-01-15 11:27:27 +0800

    Update example.js

--- DEMO/example.js
+++ DEMO/example.js
@@ -15,3 +15,21 @@ removeDuplicateItems([42, 'foo', 42, 'foo', true, true]);
 //数据计算相关
 //https://github.com/nefe/number-precision/blob/master/src/index.ts
 
+//compose 组合函数 
+
+
+
+// es6
+ const compose = (f,g) => x > f(g(x));
+
+// 网上看到的redux源码的一个compose函数:
+
+function compose(...funcs){
+  if(funcs.length === 0){
+    return arg => arg;
+  }
+  if(funcs.length === 1){
+    return funcs[0]
+  }
+  return funcss.reduce((a,b) => (...args) => a(b(...args)))
+}
