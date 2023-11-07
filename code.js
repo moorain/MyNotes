6b9fdebebc9cbe3f11472a57c51dce93829b1b79
Moorain <1062254476@qq.com>
2023-11-07 22:31:37 +0800

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,6 @@
- function findPairsWithSum(arr, targetSum) {
+//取出[1,2,3,4,5,6,7,8]中和等于9的数
+
+function findPairsWithSum(arr, targetSum) {
         const numMap = {};
         const result = [];
 
@@ -15,3 +17,21 @@
 
         return result;
       }
+//取出[1,2,3,4,5,6,7,8,8,1,1,8]中和等于9的数，数字有重复
+const func = (arr, num = 9) => {
+        const numMap = {};
+        const result = [];
+
+        for (let i = 0; i < arr.length; i++) {
+          const current = arr[i];
+          const last = num - current;
+
+          if (numMap[last]?.length) {
+            result.push([current, numMap[last].shift()]);
+          }
+
+          numMap[current] = numMap[current] || [];
+          numMap[current].push(current);
+        }
+        return result;
+      };
